4c719c9385eabaa6f089e5ad80158668cb8d30e5
Nathan Gorski <gorski@darwin-fe2.lanl.gov>
2025-08-06 11:45:06 -0600

    added more trial scripts

--- bench_all.py
+++ bench_all.py
@@ -3,6 +3,9 @@ import ase
 import time
 import torch
 import csv
+import statistics
+import sys
+import warnings
 
 from model_maker import make_model
 
@@ -15,22 +18,41 @@ from ase import units
 
 import os
 
-os.environ['TRITON_PRINT_AUTOTUNING'] = '1'
+# Uncomment the line below in order to print out the autotuning results (can be nice for knowing how far into autotuning you are)
+# os.environ['TRITON_PRINT_AUTOTUNING'] = '1'
 
 def main():
-    out_file = "./results/time_all.csv"
-    data_folder = "bench_data"
 
+    # I find it easier to set parameters as variables than to pass them in as command line arguments.
+
+    # l_max and n_max are as they are in HopInvariantsLayer.py
+    l_max=3
+    n_max=4
+
+    num_spin_up = 3 # How many times we run the calculator on each molecule before evaluating to get spin-up times (compiling etc.) out of the way.
+    num_repeat = 10 # how many times we evaluate the timing on each molecule.
+    size_thresh = 10000 # only time molecules that have less atoms than this threshold. Set this equal to -1 to time all molecules in the data folder.
+
+    # list of (sensitivites, features) combinations that we want to test out.
+    # nu_and_b_list = [(20,128), (20, 200), (20,256), (20,300), (20,400), (20,512), (40,128), (40,200), (40,256), (40,300)]
+    nu_and_b_list = [(20,128), (20,200)]
+
+    out_file = "../results/time_all.csv" # where should we write the results
+    data_folder = "../bench_data" # folder containing the molecules that we are testing (in .pdb format)
+
+    suppress_model_creation_prints = True # Whether we want to suppress the print statements from creating models that say things like "determined inputs" etc.
+    warnings.filterwarnings("ignore") # uncomment this line in order to ignore warnings that are coming from HIP-HOP-NN (the main warning that gets printed out is saying that it is in a beta stage)
+
+    stdout = sys.stdout
+
+    # open the results file and write the header
     outf = open(out_file, "a")
     writer = csv.writer(outf)
     writer.writerow(["l_max", "n_max", "nu", "b", "molecule", "time default", "time polys", "time grad fused", "time polys grad fused", "time env+grads fused", "time polys env+grads fused"])
 
-    l_max=3
-    n_max=4
-
-    num_repeat = 10
-    size_thresh = 10000
-
+    # load in the molecules from the data folder and load them in using ase.
+    # This creates an array, test_molecules. Each entry is a tuple of the
+    # molecule's filename, and then an object of the molecule.
     test_molecules = []
     for f in os.listdir(data_folder):
         if os.path.isfile(f"{data_folder}/{f}"):
@@ -38,51 +60,71 @@ def main():
             a = read_proteindatabank(inf,index=0)
             test_molecules.append((f,a))
 
-    for nu in [20,40]:
-        for b in [128,200,256,300,400,512]:
+    # iterate over different model sizes.
+    for nu, b in nu_and_b_list:
 
-            for m in test_molecules:
+        # time each molecule.
+        for m in test_molecules:
 
-                if size_thresh == -1 or len(m[1]) < size_thresh:
-                    config_times = {}
+            if size_thresh == -1 or len(m[1]) < size_thresh:
+                times = {} # stores the times that we will write to file
 
-                    for config in [(False,False,False)]:#,(True,False,False),(False,False,True),(True,False,True),(False,True,True),(True,True,True)]:
+                # Ablation testing:
+                # Each tuple specifies which of my optimizations are implemented comapred to the default.
+                # The first entry is set to True to use polynomial invariants, and False to use default inariants.
+                # The second entry is set to True to use the tensor message passing implementation for the forward pass, and False otherwise.
+                # The third entry is set to True to use the tensor message passing implementation for the gradient computation, and False otherwise.
 
-                        times = []
+                # Throughout these trials, we run m[1].rattle(1e-6). This slightly perturbs the molecules with a standard deviation of 1e-6.
+                # In doing so, the molecules are slightly shifted, which means that the results of the calculator will not be cached. On the other
+                # hand, because the perturbation is so small, the graph will not need to be rebuilt. This allows us to actually re-run the forward pass
+                # (just without the graph being rebuilt).
+                for config in [(False,False,False), (True,False,False), (False,False,True), (True,False,True), (False,True,True), (True,True,True)]:
 
-                        model, _ = make_model(nu, b, l_max, n_max, *config)
-                        c = calculator_from_model(model, en_unit = units.eV, dist_unit = units.Ang).to('cuda')
-                        m[1].calc = c
+                    times_for_this_config = [] # only used for printing to console.
 
-                        print(f"{m[0]} ({len(m[1])}) [{config}]: 0 / {num_repeat}",end="\r")
-                        for repeat in range(3):
-                            m[1].rattle(1e-6)
-                            e = m[1].get_potential_energy()
-                            f = m[1].get_forces()
+                    # Create the model, obtain the calculator, and set it as the molecule's calculator.
+                    if suppress_model_creation_prints:
+                        sys.stdout = open(os.devnull, "w")
+                    model, _ = make_model(nu, b, l_max, n_max, *config)
+                    sys.stdout = stdout
 
-                        for x in range(num_repeat):
-                            print(f"{m[0]} ({len(m[1])}) [{config}]: {x+1} / {num_repeat}",end="\r")
+                    c = calculator_from_model(model, en_unit = units.eV, dist_unit = units.Ang).to('cuda')
+                    m[1].calc = c
 
-                            m[1].rattle(1e-6)
+                    # run the calculator to get spin-up times out of the way.
+                    print(f"{m[0]} ({len(m[1])}) [{config}]: spinning up...",end="\r")
+                    for repeat in range(num_spin_up):
+                        m[1].rattle(1e-6)
+                        e = m[1].get_potential_energy()
+                        f = m[1].get_forces()
 
-                            torch.cuda.synchronize()
-                            t1 = time.time()
+                    # Run the calculator the number of times that 
+                    for x in range(num_repeat):
+                        print(f"{m[0]} ({len(m[1])}) [{config}]: {x+1} / {num_repeat}                                                ",end="\r")
 
-                            e = m[1].get_potential_energy()
-                            f = m[1].get_forces()
+                        m[1].rattle(1e-6)
 
-                            torch.cuda.synchronize()
-                            t2 = time.time()
-                            print(t2-t1)
+                        torch.cuda.synchronize()
+                        t1 = time.time()
 
-                            times.append(t2-t1)
+                        e = m[1].get_potential_energy()
+                        f = m[1].get_forces()
 
-                        med_time = statistics.median(times) / len(m[1])
-                        print(f"{m[0]} ({len(m[1])}) [{config}]: {med_time}                                          ")
-                        config_times[config] = med_time
-                    
-                    writer.writerow([ l_max, n_max, nu, b, m[0], config_times[(False,False,False)], config_times[(True,False,False)], config_times[(False,False,True)], config_times[(True,False,True)], config_times[(False,True,True)], config_times[(True,True,True)]] )
-                    outf.flush()
+                        torch.cuda.synchronize()
+                        t2 = time.time()
+
+                        times[ (x, *config) ] = t2-t1
+                        times_for_this_config.append(t2-t1)
+
+                    med_time = statistics.median(times_for_this_config)
+                    print(f"{m[0]} ({len(m[1])}) [{config}]: {med_time}                                          ")
+
+                # write times to the file:
+                for x in range(num_repeat):
+                    writer.writerow([ l_max, n_max, nu, b, m[0], times[(x,False,False,False)], times[(x,True,False,False)], times[(x,False,False,True)], times[(x,True,False,True)], times[(x,False,True,True)], times[(x,True,True,True)]] )
+
+                outf.flush()
 
 if __name__ == "__main__":
     main()
---
+++ evaluate_calculator.py
@@ -0,0 +1,156 @@
+from mace.calculators import mace_off
+from hippynn.experiment.serialization import load_model_from_cwd
+from hippynn.interfaces.ase_interface.calculator import calculator_from_model
+import ase
+from ase import units
+import numpy as np
+import math
+import csv
+import os
+
+from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
+from ase.md.verlet import VelocityVerlet
+
+import time
+import torch
+import numpy
+
+# This file evaluates the accuracy of an ASE calculator (either MACE-OFF or ours)
+# on the test data that is from MACE. It compute the RMSE of both energy and force
+# computation on the mace test data, and disaggregates the results based on
+# the molecule type (as is done in the MACE-OFF paper). It also computes the average
+# molecule size per category.
+if __name__ == "__main__":
+
+    # set to an integer to stop evaluation once that many molecules are reached.
+    # For example, if this is set to 100, only the first 100 molecules will be
+    # evaluated. Primarily used for debugging.
+    # Otherwise, set this equal to float('inf).
+    kill_early = float('inf')
+    
+    # Set this equal to 'small,' 'medium,' or 'large' to evaluate MACE-OFF
+    # small, medium, or large, respectively. Otherwise, specify the folder
+    # corresponding to a hippynn calculator that you would like to evaluate.
+    calc_name = "medium"
+
+    # What file should we store the trial's results to. If the file already exists, it will
+    # append the results as another row of the file. Otherwise, it will write the header and data.
+    out_file = "results.csv"
+
+    # The data should all be formatted using the npy format for hippynn 
+    # (see the documentation) in one folder. The prefix should specify
+    # the folder and names of the files. In the examples given here,
+    # the data files are '../testData/mace-test-data-Z.npy" etc.
+
+    # The categories are identified by a number (in a numpy array)
+    # The names of the categories should be stored in (prefix)-Category-Names.txt
+    prefix = "../testData/mace-test-data"
+    species = np.load(f"{prefix}-Z.npy")
+    location = np.load(f"{prefix}-R.npy")
+    energy = np.load(f"{prefix}-T.npy")
+    forces = np.load(f"{prefix}-F.npy")
+    categories = np.load(f"{prefix}-C.npy")
+    num_categories = np.max(categories)+1
+
+    # Everything else does not correspond to parameters.
+
+    # Load in the calculator:
+    if calc_name in ["small", "medium", "large"]:
+        calc = mace_off(model=calc_name,dispersion=False, dtype=torch.float32)
+    else:
+        # change directory to the hippynn model that we are loading.
+        # then load from cwd and change the directory back.
+        cwd = os.getcwd()
+        os.chdir(calc_name)
+        model = load_model_from_cwd()
+        calc = calculator_from_model(model, en_unit = units.eV, dist_unit = units.Ang).to('cuda')
+        os.chdir(cwd)
+
+    # Store results by category.
+    mol_sizes = [0] * num_categories
+    e_se_peratom = [0] * num_categories
+    f_se_peratom = [0] * num_categories
+    n_mols_eval = [0] * num_categories
+
+    num_mols_to_test = min(species.shape[0], kill_early)
+
+    # iterate through molecules.
+    for i in range(num_mols_to_test):
+        print(f"{i+1} / {num_mols_to_test}                      ", end="\r")
+        # pull relevant information
+        gt_energy = energy[i]
+        gt_forces = forces[i]
+        category = categories[i]
+
+        # Get species and locations in list form in order to create ASE object.
+        species_list = list(species[i])
+        if 0.0 in species_list:
+            num_atoms = species_list.index(0.0)
+        else:
+            num_atoms = len(species_list)
+        species_list = species_list[:num_atoms]
+
+        location_list = location[i,:num_atoms,:]
+
+        # Make ase object and set calculator
+        atoms = ase.Atoms(species_list, location_list)
+        atoms.calc = calc
+
+        # Predict force and energy
+        pred_energy = atoms.get_total_energy()
+        pred_forces = atoms.get_forces()
+
+        # Remove padding from gt_forces
+        gt_forces = gt_forces[:num_atoms,:]
+
+        # Update information to lists.
+        mol_sizes[category] += num_atoms
+        e_se_peratom[category] += ((pred_energy-gt_energy)/num_atoms)**2
+        f_se_peratom[category] += np.sum(((pred_forces-gt_forces)/num_atoms)**2)
+        n_mols_eval[category] += 1
+
+    # Load in the category names for evaluation.
+    category_names = []
+    category_names_file = open(f"{prefix}-Category-Names.txt", "r")
+    for name in category_names_file:
+        category_names.append(name.strip())
+
+    # Compute the RMSEs and print results.
+    file_header = ["model name"]
+    if calc_name in ["small", "medium", "large"]:
+        file_results = ["MACE-OFF " + calc_name]
+    else:
+        file_results = [calc_name]
+    if kill_early != float('inf'):
+        file_results[0] += f" ({kill_early})"
+
+    for i in range(num_categories):
+        if n_mols_eval[i] == 0:
+            continue
+
+        avg_mol_size = mol_sizes[i] / n_mols_eval[i]
+        e_rmse_peratom = math.sqrt(e_se_peratom[i] / n_mols_eval[i])
+        f_rmse_peratom = math.sqrt(f_se_peratom[i] / n_mols_eval[i])
+
+        print(f"{category_names[i]} ({n_mols_eval[0]} molecules @ {avg_mol_size} atoms (on avg))")
+        print(f"E RMSE (ev): {e_rmse_peratom}")
+        print(f"F RMSE (ev/Å): {f_rmse_peratom}")
+
+        file_header.append(f"{category_names[i]} E RMSE (ev)")
+        file_header.append(f"{category_names[i]} F RMSE (ev/Å)")
+        file_header.append(f"{category_names[i]} Avg Mol. Size")
+
+        file_results.append(e_rmse_peratom)
+        file_results.append(f_rmse_peratom)
+        file_results.append(avg_mol_size)
+
+        print()
+
+    # Write results to file.
+    write_header = not os.path.isfile(out_file)
+    outf = open(out_file, "a")
+    writer = csv.writer(outf)
+    if write_header:
+        writer.writerow(file_header)
+    writer.writerow(file_results)
+    outf.close()    
