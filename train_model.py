f2322b64b8be15afb386969d4e9f9fa7c09dd936
Nathan Gorski <gorski@darwin-fe2.lanl.gov>
2025-08-06 14:50:50 -0600

    annotated train_model.py

--- train_model.py
+++ train_model.py
@@ -3,29 +3,31 @@ import torch
 import hippynn
 import os
 
-# This file is used for training models. I did not get around to writing the documentation yet. Please come back later.
+# This file is used for creating and training HIP-HOP-NN models. It only works with a hippynn.DirectoryDatabase
+# for the data, although this could be expanded to other types of databases as needed.
 
-def make_model(network_params, tensor_model, tensor_order, tensor_factors, atomization_consistent):
+# Returns the henergy, force and species nodes from a given tensor model type with given n_max and l_max.
+def get_primary_nodes(network_params, tensor_model, l_max, n_max, atomization_consistent):
     from hippynn.graphs import inputs, networks, targets, physics
 
-    if tensor_order == 0 or tensor_model == "NONE":
+    if l_max == 0 or tensor_model == "NONE":
         net_class = networks.Hipnn
     else:
         if tensor_model == "TS":
             net_class = {
                 1: networks.HipnnVec,
                 2: networks.HipnnQuad,
-            }[tensor_order]
+            }[l_max]
         elif tensor_model == "HOP":
             net_class = networks.HipHopnn
-            network_params["l_max"] = tensor_order
-            network_params["n_max"] = tensor_factors
+            network_params["l_max"] = l_max
+            network_params["n_max"] = n_max
 
     species = inputs.SpeciesNode(db_name="Z")
     positions = inputs.PositionsNode(db_name="R")
 
     network = net_class("hipnn_model", (species, positions), module_kwargs=network_params)
-        
+
     if not atomization_consistent:
         henergy = targets.HEnergyNode("HEnergy", network)
     else:
@@ -36,10 +38,10 @@ def make_model(network_params, tensor_model, tensor_order, tensor_factors, atomi
     return henergy, force, species
 
 
-def make_loss(henergy, force, species, force_training, force_percent):
-    """
-    Build the loss graph for energy and force error.
-    """
+# Creates the loss nodes for training from the given henergy, force, and species node.
+# force_percent is a number from 0-1 that states how much force should be weighted in
+# the loss function (set force_percent = 0.0 to not train on forces at all).
+def make_loss(henergy, force, species, force_percent):
     from hippynn.graphs.nodes.loss import MSELoss, MAELoss, Rsq, Mean
     from hippynn.graphs import physics
 
@@ -62,14 +64,14 @@ def make_loss(henergy, force, species, force_training, force_percent):
 
     losses["EnergyTotal"] = losses["T-RMSE"] + losses["T-MAE"]
     losses["LossTotal"] = losses["EnergyTotal"] + losses["T-Hier"]
-    if force_training:
+    if force_percent != 0.0:
         losses.update(force_losses)
         losses["ForceTotal"] = losses["F-RMSE"] + losses["F-MAE"]
         losses["LossTotal"] = 2*( (1 - force_percent) * losses["LossTotal"] + force_percent * losses["ForceTotal"] )
 
     return losses
 
-# completely gutting the actual ani1x function and substituting it with a much simpler one.
+# creates a database given relevant information about the database.
 def load_db(db_info, name, directory, quiet, test_size, valid_size, seed):
 
     from hippynn.databases import DirectoryDatabase
@@ -81,7 +83,6 @@ def load_db(db_info, name, directory, quiet, test_size, valid_size, seed):
         "test_size": test_size,
         "valid_size": valid_size,
         "seed": seed,
-        # How many samples from the training set to use during evaluation
         **db_info,  # Adds the inputs and targets names from the model as things to load
     }
 
@@ -89,11 +90,8 @@ def load_db(db_info, name, directory, quiet, test_size, valid_size, seed):
 
     return database
 
-
+# Sets up objects that are required for the experiment to run.
 def setup_experiment(training_modules, device, batch_size, init_lr, patience, max_epochs, stopping_key):
-    """
-    Set up the training run.
-    """
     from hippynn.experiment.controllers import RaiseBatchSizeOnPlateau, PatienceController
 
     optimizer = torch.optim.Adam(training_modules.model.parameters(), lr=init_lr)
@@ -120,48 +118,16 @@ def setup_experiment(training_modules, device, batch_size, init_lr, patience, ma
     )
     return setup_params
 
-
-ANI1X_DSETS_KEYS = [
-    "hf_tz.energy",
-    "coordinates",
-    "tpno_ccsd(t)_dz.corr_energy",
-    "wb97x_dz.hirshfeld_charges",
-    "wb97x_tz.mbis_charges",
-    "wb97x_tz.forces",
-    "mp2_tz.corr_energy",
-    "npno_ccsd(t)_tz.corr_energy",
-    "wb97x_tz.mbis_volumes",
-    "wb97x_tz.energy",
-    "wb97x_tz.dipole",
-    "wb97x_tz.mbis_octupoles",
-    "wb97x_tz.mbis_quadrupoles",
-    "mp2_qz.corr_energy",
-    "wb97x_tz.mbis_dipoles",
-    "wb97x_dz.cm5_charges",
-    "path",
-    "atomic_numbers",
-    "hf_qz.energy",
-    "mp2_dz.corr_energy",
-    "wb97x_dz.dipole",
-    "npno_ccsd(t)_dz.corr_energy",
-    "wb97x_dz.energy",
-    "hf_dz.energy",
-    "wb97x_dz.quadrupole",
-    "ccsd(t)_cbs.energy",
-    "wb97x_dz.forces",
-]
-
-
 def main(args):
+    # Configure GPU settings
     torch.manual_seed(args.seed)
     if args.use_gpu:
         torch.cuda.set_device(args.gpu)
     torch.set_default_dtype(torch.float32)
 
     hippynn.settings.WARN_LOW_DISTANCES = False
-    # if args.noprogress:
-    #     hippynn.settings.PROGRESS = None
 
+    # set up network parameters
     netname = f"{args.tag}_GPU{args.gpu}"
     network_parameters = {
         "possible_species": [0,1,6,7,8,9,15,16,17,35,53],
@@ -178,6 +144,7 @@ def main(args):
         with hippynn.tools.log_terminal("training_log.txt", "wt"):
 
             if os.path.isfile("./best_model.pt") and not args.force_restart:
+                # If the model has already been trained, then resume training.
 
                 from hippynn.experiment.serialization import load_checkpoint_from_cwd
                 from hippynn.experiment import train_model
@@ -186,8 +153,10 @@ def main(args):
                 train_model(**check, callbacks=None, batch_callbacks=None)
 
             else:
+                # If the model has not yet been trained, then create a new model and traing it.
 
-                henergy, force, species = make_model(
+                # Create model and loss functions
+                henergy, force, species = get_primary_nodes(
                     network_parameters,
                     tensor_model=args.tensor_model,
                     tensor_order=args.tensor_order,
@@ -198,7 +167,7 @@ def main(args):
                 henergy.mol_energy.db_name = "T"
                 force.db_name = "F"
 
-                validation_losses = make_loss(henergy, force, species, force_training=args.force_training, force_percent=args.force_percent)
+                validation_losses = make_loss(henergy, force, species, force_percent=args.force_percent)
 
                 train_loss = validation_losses["LossTotal"]
 
@@ -206,6 +175,7 @@ def main(args):
 
                 training_modules, db_info = assemble_for_training(train_loss, validation_losses)
 
+                # Load data
                 database = load_db(
                     db_info,
                     args.db_name,
@@ -219,6 +189,8 @@ def main(args):
                 from hippynn.pretraining import hierarchical_energy_initialization
 
                 hierarchical_energy_initialization(henergy, database, trainable_after=False)
+
+                # Setup training apparatus and begin training.
 
                 patience = args.patience
                 if args.use_ccx_subset:
@@ -247,7 +219,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     from argparse import BooleanOptionalAction
 
-    parser.add_argument("--tag", type=str, default="TEST_MODEL_ANI1X", help="name for run")
+    parser.add_argument("--tag", type=str, default="TEST_MODEL", help="name for run")
     parser.add_argument("--gpu", type=int, default=0, help="which GPU to run on, if any")
     parser.add_argument(
         "--use-gpu",
@@ -278,8 +250,6 @@ if __name__ == "__main__":
     parser.add_argument("--tensor_factors", type=int, default=4, help="number of factors used (in HIP-HOP-NN only)")
     parser.add_argument("--atomization_consistent", type=bool, default=False)
 
-    parser.add_argument("--force_training", action=BooleanOptionalAction, default=True, help="Use force training.")
-
     parser.add_argument("--batch_size", type=int, default=256)
     parser.add_argument("--init_lr", type=float, default=1e-3)
     parser.add_argument("--patience", type=int, default=5)
@@ -304,15 +274,8 @@ if __name__ == "__main__":
     parser.add_argument("--test_size", type=float, default=0.1, help="Percentage of the dataset used for testing (decimal between 0 and 1)")
     parser.add_argument("--valid_size", type=float, default=0.1, help="Percentage of dataset used for validation (decimal between 0 and 1)")
     parser.add_argument("--force_restart", action=BooleanOptionalAction, default=False, help="Restart training from beginning.")
-    parser.add_argument("--profile", action=BooleanOptionalAction, default=False, help="Should I profile the code?")
-    parser.add_argument("--trace_name", type=str, default="trace.json", help="Name of the chrome trace file that is exported.")
     parser.add_argument("--force_percent", type=float, default=0.5, help="What percentage of the loss is due to the force.")
 
     args = parser.parse_args()
 
-    if args.profile:
-        hippynn.create_profiler()
-
     main(args)
-
-    hippynn.profiler_export_to_chrome(args.trace_name)
